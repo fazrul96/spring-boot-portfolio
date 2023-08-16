8258849a87d1175a194d27163cdeaa55ff06b78f
Fazrul Romli <muhammad.f.romli@accenture.com>
2023-08-16 11:01:28 +0800

    initial spring boot

--- spring-boot/src/test/java/com/example/demo/ApplicationTests.java
+++ spring-boot/src/test/java/com/example/demo/ApplicationTests.java
@@ -1,10 +1,10 @@
-package javabackend;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PortfolioApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
