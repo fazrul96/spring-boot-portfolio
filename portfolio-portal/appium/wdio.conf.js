bda33071df623d0f9a01eea1d411393453edafec
Fazrul_Romli[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C <muhammad.f.romli@accenture.com>
2023-09-21 14:48:21 +0800

    update wdio to autogenerate report

--- portfolio-portal/appium/wdio.conf.js
+++ portfolio-portal/appium/wdio.conf.js
@@ -1,3 +1,5 @@
+const allure = require('allure-commandline')
+
 exports.config = {
     runner: process.env.TEST_ENV,
     port: parseInt(process.env.APPIUM_PORT),
@@ -20,10 +22,13 @@ exports.config = {
     connectionRetryCount: 3,
     services: ['appium'],
     framework: 'cucumber',
-    reporters: ['spec', ['allure', {
+    reporters:     
+    ['spec', 
+    ['allure', {
         outputDir: './allure/allure-results',
         disableWebdriverStepsReporting: false,
         disableWebdriverScreenshotsReporting: false,
+        reportedEnvironmentVars: true
     }]],
     cucumberOpts: {
         // <string[]> (file/dir) require files before executing features
@@ -43,10 +48,32 @@ exports.config = {
         // <boolean> fail if there are any undefined or pending steps
         strict: false,
         // <string> (expression) only execute the features or scenarios with tags matching the expression
-        tags: '@SmokeTest', // '@SmokeTest or @SanityTest'
+        tags: '@SanityTest', // '@SmokeTest or @SanityTest' '@TestingTest'
         // <number> timeout for step definitions
         timeout: 60000,
         // <boolean> Enable this config to treat undefined definitions as warnings.
         ignoreUndefinedDefinitions: false
+    },
+    // Autogenerate Report
+    onComplete: function() {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure/allure-results', '--clean', '-o', 'allure/allure-report']);
+
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
     }
 }
