618bc70d1fccae682b263d867a5611a613f22de5
Fazrul_Romli <muhammad.f.romli@accenture.com>
2023-10-30 12:11:11 +0800

    implement jacoco and add sample test

---
+++ spring-boot/src/main/java/com/example/demo/Calculator.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+
+    public int square(int a) {
+        return a * a;
+    }
+}
